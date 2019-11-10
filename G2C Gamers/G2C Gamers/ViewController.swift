712012003990a320ad84991f226bc910a496d6ab
madusha perera <madupiz@gmail.com>
2019-11-10 17:45:41 +0530

    Created the git Repo and init the project
    initialized the project

---
+++ G2C Gamers/G2C Gamers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G2C Gamers
+//
+//  Created by Madusha on 11/10/19.
+//  Copyright © 2019 Madusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
