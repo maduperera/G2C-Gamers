4bde4667fef9c0d10b5523c5b27e3ca8389162e6
madusha perera <madupiz@gmail.com>
2019-11-16 21:41:25 +0530

    Finalizing Favourite screen

--- G2C Gamers/G2C Gamers/Controller/FavouritesViewController.swift
+++ G2C Gamers/G2C Gamers/Controller/FavouritesViewController.swift
@@ -13,9 +13,9 @@ class FavouritesViewController: UIViewController {
     var items : [Game]?
     @IBOutlet weak var tblView: UITableView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // read all games stored
         //items = DBHandler.sharedInstance.readAll() //.filter({(game:Game) in game.favourite == true })
 //        print("favouriteItems :\(items)")
@@ -25,8 +25,9 @@ class FavouritesViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         // read all games stored
         items = DBHandler.sharedInstance.readAll()
-        print("favouriteItems :\(items)")
         tblView.reloadData()
+        guard let count = items?.count else {return}
+        self.title = "Favourites (\(String(count)))"
     }
 
 }
@@ -34,15 +35,30 @@ class FavouritesViewController: UIViewController {
 
 
 extension FavouritesViewController :  UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavouriteCell.reuseIdentifier, for: indexPath) as? FavouriteCell ?? FavouriteCell()
+        
+        guard let items = items else {return cell}
+        cell.configureWith(items[indexPath.item])
+        return cell
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let items = items else {return 0}
         return items.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = items?[indexPath.row].name
-        return cell
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let gameDetailsVC = GameDetailsViewController.instantiate()
+        guard let items = items else { return }
+        navigationItem.searchController?.isActive = false
+        gameDetailsVC.inititialize(game: items[indexPath.item])
+        navigationController?.pushViewController(gameDetailsVC, animated: true)
         
     }
     
---
+++ G2C Gamers/G2C Gamers/View/Cells/FavouriteCell.swift
@@ -0,0 +1,44 @@
+//
+//  gameCell.swift
+//  G2C Gamers
+//
+//  Created by Madusha on 11/12/19.
+//  Copyright © 2019 Madusha. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class FavouriteCell: UITableViewCell {
+    
+    public static let reuseIdentifier = "FavouriteCell"
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var imgThumb: UIImageView!
+    @IBOutlet weak var lblMetaCriticsCount: UILabel!
+    @IBOutlet weak var lblGenere: UILabel!
+    
+    public func configureWith(_ game: Game) {
+        if let name = game.name{
+            lblTitle.text = name
+        }
+        if let metacritic = game.metacritic{
+            lblMetaCriticsCount.text = String(metacritic)
+        }
+        guard let imagePath = game.background_image else {return}
+        imgThumb.kf.setImage(with: URL(string: imagePath),
+                             options: [.transition(.fade(0.3))])
+        
+        guard let genres = game.genres else {return}
+        lblGenere.text = aggregateGenres(genres: genres)
+        
+    }
+    
+    // aggregate the genres to one line
+    func aggregateGenres(genres: [Game.Genre]) -> String{
+        let names = genres.map({ $0.name ?? "" })
+        return names.joined(separator: ", ")
+    }
+    
+}
+
